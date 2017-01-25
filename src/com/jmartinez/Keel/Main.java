7c63d2b02d25f2431c7072da0c9d54eeb50a4566
Javier Martínez <javier.martinez.jimenez@tecsisa.com>
2017-01-25 17:55:01 +0100

    working

--- src/com/jmartinez/Keel/Main.java
+++ src/com/jmartinez/Keel/Main.java
@@ -1,6 +1,14 @@
-package com.jmartinez;
+package com.jmartinez.Keel;
+
+import com.jmartinez.Keel.Fuzzy_Rule_Learning.Algorithms.ClassifierFuzzyWangMendel;
+import com.jmartinez.Keel.Shared.Parsing.ProcessConfig;
+import core.*;
 
 public class Main {
+
+
+    //The Randomize object used in this class
+    static Randomize rand;
 
     /**
      * <p>
@@ -10,10 +18,13 @@ public class Main {
      *              path of the algorithm's parameters file must be given.
      */
     public static void main(String[] args) {
+        System.out.println("Me estoy ejecutando: Algoritmo Wang&Mendel");
         boolean tty=false;
         ProcessConfig pc=new ProcessConfig();
-        System.out.println("Reading configuration file: "+args[0]);
-        if (pc.fileProcess(args[0])<0) return;
+        System.out.println("Reading configuration file: ");
+        String file = "/Users/Javi/development/WangMendelAlgorithm/resources/config0s0.txt";
+        int aux = pc.fileProcess(file);
+        if (aux<0) return;
         int algo=pc.parAlgorithmType;
         rand=new Randomize();
         rand.setSeed(pc.parSeed);
